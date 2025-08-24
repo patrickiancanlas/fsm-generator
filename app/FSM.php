ab0c7f5da59f24422390088facb78bc4fd2137ca
Patrick Canlas <patrickian.canlas@gmail.com>
2025-08-24 01:35:37 -0700

    add transition function and returning state function for FSM

--- app/FSM.php
+++ app/FSM.php
@@ -42,21 +42,21 @@ class FSM
         }
     }
 
-    public function addStateTransition(string $stateName, string $input, string $newState)
+    public function addStateTransition(string $stateName, string $input, string $resultState)
     {
         if ($this->doesStateExists(name: $stateName)) {
             throw new Exception('This state does not exist');
         }
 
         if ($this->doesInputExists(input: $input)) {
-            throw new Exception('Wrong input');
+            throw new Exception('Input not recognized in this FSM');
         }
 
-        if ($this->doesStateExists(name: $newState)) {
-            throw new Exception('New state does not exist');
+        if ($this->doesStateExists(name: $resultState)) {
+            throw new Exception('Result state does not exist');
         }
 
-        $this->states[$stateName]->addTransition(input: $input, newState: $newState);
+        $this->states[$stateName]->addTransition(input: $input, newState: $resultState);
     }
 
     public function removeStateTransition(string $stateName, string $input) {
@@ -79,5 +79,30 @@ class FSM
     private function doesInputExists(string $input)
     {
         return in_array($input, $this->inputs);
+    }
+
+    public function runTransition(string $input)
+    {
+        if ($this->doesInputExists($input)) {
+            throw new Exception('Input not recognized in this FSM');
+        }
+
+        $currentState = $this->states[$this->currentStateName];
+
+        // Set new state for FSM
+        $this->currentStateName = $currentState->getResultState(input: $input);
+    }
+
+    public function returnCurrentState()
+    {
+        $currentState = $this->states[$this->currentStateName];
+        if (!$currentState->isFinal) {
+            throw new Exception('This state is not final');
+        }
+
+        return [
+            'state_name' => $currentState->name,
+            'state_output' => $currentState->output
+        ];
     }
 }
