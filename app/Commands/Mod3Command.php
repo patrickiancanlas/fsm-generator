1e52e81d48f779aeab451fb639ec10a8fb7900a3
Patrick Canlas <patrickian.canlas@gmail.com>
2025-08-24 02:11:25 -0700

    mod3 implementation and command

---
+++ app/Commands/Mod3Command.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Commands;
+
+use App\Mod3;
+use Illuminate\Console\Scheduling\Schedule;
+use LaravelZero\Framework\Commands\Command;
+
+class Mod3Command extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:mod3-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $input = $this->ask('Enter binary integer string to get remainder');
+        if (!$this->isStringBinary($input)) {
+            $this->fail('Input is not a binary number');
+        }
+
+        $mod3 = new Mod3();
+        $this->info('The remainder is ' . $mod3->run(binaryInteger: $input));
+        return 0;
+    }
+
+    /**
+     * Define the command's schedule.
+     */
+    public function schedule(Schedule $schedule): void
+    {
+        // $schedule->command(static::class)->everyMinute();
+    }
+
+    private function isStringBinary($stringNumber)
+    {
+        return preg_match('/^[01]+$/', $stringNumber) === 1;
+    }
+}
---
+++ app/Mod3.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use App\FSM;
+
+class Mod3
+{
+    public FSM $fsm;
+
+    public function __construct()
+    {
+        $inputs = ['0', '1'];
+
+        $this->fsm = new FSM($inputs);
+
+        // Add states
+        $this->fsm->addState(name: 'S0', output: '0', isFinal: true, isInitial: true);
+        $this->fsm->addState(name: 'S1', output: '1', isFinal: true);
+        $this->fsm->addState(name: 'S2', output: '2', isFinal: true);
+
+        // Add state transition for S0
+        $this->fsm->addStateTransition(stateName: 'S0', input: '0', resultState: 'S0');
+        $this->fsm->addStateTransition(stateName: 'S0', input: '1', resultState: 'S1');
+
+        // Add state transition for S1
+        $this->fsm->addStateTransition(stateName: 'S1', input: '0', resultState: 'S2');
+        $this->fsm->addStateTransition(stateName: 'S1', input: '1', resultState: 'S0');
+
+        // Add state transition for S2
+        $this->fsm->addStateTransition(stateName: 'S2', input: '0', resultState: 'S1');
+        $this->fsm->addStateTransition(stateName: 'S2', input: '1', resultState: 'S2');
+    }
+
+    public function run(string $binaryInteger)
+    {
+        $inputArray = str_split($binaryInteger);
+        foreach ($inputArray AS $input) {
+            $this->fsm->runTransition(input: $input);
+        }
+
+        return $this->fsm->returnFinalState()['output'];
+    }
+}
