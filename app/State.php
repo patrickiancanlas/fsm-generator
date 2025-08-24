be350beec0f470e12fe9924b2dc2ed891c7eb2a0
Patrick Canlas <patrickian.canlas@gmail.com>
2025-08-24 01:36:18 -0700

    add returning result state function

--- app/State.php
+++ app/State.php
@@ -2,25 +2,36 @@
 
 namespace App;
 
+use Exception;
+
 class State
 {
-    public array $transitions = [];
+    public array $transitionStates = [];
 
     public function __construct(
         public readonly string $name,
         public readonly string $output,
-        public readonly bool $isFinal = false
+        public readonly bool $isFinal = true
     )
     {
     }
 
-    public function addTransition(string $input, string $newState)
+    public function addTransition(string $input, string $resultState)
     {
-        $this->transitions[$input] = $newState;
+        $this->transitionStates[$input] = $resultState;
     }
 
     public function removeTransition(string $input)
     {
-        unset($this->transitions[$input]);
+        unset($this->transitionStates[$input]);
+    }
+
+    public function getResultState($input)
+    {
+        if (array_key_exists($input, $this->transitionStates)) {
+            return $this->transitionStates[$input];
+        }
+
+        throw new Exception('Transition not set for this state input');
     }
 }
