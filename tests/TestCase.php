243680f8cb954ca977ad075acc81ffb49cd35ba4
Patrick Canlas <patrickian.canlas@gmail.com>
2025-08-28 11:47:45 -0700

    convert laravel zero to full laravel application, include docker compose file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,9 @@
 
 namespace Tests;
 
-use LaravelZero\Framework\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase {}
+abstract class TestCase extends BaseTestCase
+{
+    //
+}
